6efe61da7d4803fb7fb864ac73802379bf8c5229
Roxaneryz <roxane5666@yahoo.gr>
2024-02-19 20:52:17 +0200

    hw12

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = '42454875-eb3549610f297412779ff13b6'; 
+
+const fetchImages = async (query, page = 1, perPage = 15) => {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        page: page,
+        per_page: perPage,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+};
+
+export { fetchImages };
---
+++ src/js/render-functions.js
@@ -0,0 +1,24 @@
+const renderImages = (images) => {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = ''; 
+    
+    images.forEach((image) => {
+      const imgElement = document.createElement('img');
+      imgElement.src = image.previewURL;
+      imgElement.alt = image.tags;
+      gallery.appendChild(imgElement);
+    });
+  };
+  
+  const renderLoadMoreButton = (callback) => {
+    const loadMoreBtn = document.querySelector('.load-more');
+    loadMoreBtn.style.display = 'block'; 
+    loadMoreBtn.addEventListener('click', callback);
+  };
+  
+  const hideLoadMoreButton = () => {
+    const loadMoreBtn = document.querySelector('.load-more');
+    loadMoreBtn.style.display = 'none'; 
+  };
+  
+  export { renderImages, renderLoadMoreButton, hideLoadMoreButton };
